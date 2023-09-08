aff805e015095ab5793529c57aac3ae156a87306
Daniel Gonzalez <danielgonzalezrivera2002@gmail.com>
2023-09-08 16:55:40 -0500

    avance del GameBoard

--- src/Integradora1/src/model/PipelineType.java
+++ src/Integradora1/src/model/PipelineType.java
@@ -2,9 +2,58 @@ package model;
 
 public class PipelineType {
 
+    private EnumPipeline pipelineType;
 
+    private int column, row;
 
-    public PipelineType() {
+    private GameBoard next,prev;
+
+    public PipelineType(int column, int row, int pipelineType) {
+
+        this.column = column;
+
+        this.row = row;
+
+        this.pipelineType = EnumPipeline.X;
 
     }
+
+    public void setPipelineType(int pipelineType){
+
+        switch (pipelineType){
+
+            case 1:
+
+                this.pipelineType = EnumPipeline.HORIZONTAL;
+
+                break;
+
+            case 2:
+
+                this.pipelineType = EnumPipeline.VERTICAL;
+
+                break;
+
+
+            case 3:
+
+                this.pipelineType = EnumPipeline.CIRCULAR;
+
+                break;
+
+            case 4:
+
+                this.pipelineType = EnumPipeline.F;
+
+                break;
+
+            case 5:
+
+                this.pipelineType = EnumPipeline.D;
+
+                break;
+        }
+
+    }
+
 }
